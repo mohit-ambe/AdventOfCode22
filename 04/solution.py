1106da595936627157ffea3928d6094250c9a518
Mohit Ambe <mohitambe77@gmail.com>
2022-12-04 01:35:05 -0500

    Day 4

--- 04/solution.py
+++ 04/solution.py
@@ -4,11 +4,28 @@ file.close()
 
 
 def part_one():
-    print()
+    pairs = 0
+    for line in myin:
+        one, two = line.split(",")
+        onel, oner = one.split("-")
+        twol, twor = two.split("-")
+        if int(oner) <= int(twor) and int(onel) >= int(twol) or int(twor) <= int(oner) and int(twol) >= int(onel):
+            pairs += 1
+
+    print(pairs)
 
 
 def part_two():
-    print()
+    pairs = 0
+    for line in myin:
+        one, two = line.split(",")
+        ones = set(list(range(int(one.split("-")[0]), int(one.split("-")[1]) + 1)))
+        twos = set(list(range(int(two.split("-")[0]), int(two.split("-")[1]) + 1)))
+        # print(ones, twos)
+        # print(ones & twos)
+        pairs += 1 if len(ones & twos) > 0 else 0
+
+    print(pairs)
 
 
 part_one()
