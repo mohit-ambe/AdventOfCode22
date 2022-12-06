b0c37403b1064655c5fb205631754832b1590840
Mohit Ambe <mohitambe77@gmail.com>
2022-12-06 00:19:49 -0500

    Day 6
    python better fr

--- 06/solution.py
+++ 06/solution.py
@@ -3,12 +3,18 @@ myin = [line.strip() for line in file.readlines()]
 file.close()
 
 
+def find_key(key_length):
+    for i in range(key_length - 1, len(myin[0]) - key_length):
+        if len(set([b for b in myin[0][i:i + key_length]])) == key_length:
+            return i + key_length
+
+
 def part_one():
-    print()
+    print(find_key(4))
 
 
 def part_two():
-    print()
+    print(find_key(14))
 
 
 part_one()
