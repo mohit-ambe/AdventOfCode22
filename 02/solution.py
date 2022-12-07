06aae0a0c38c39a05506debe536ca9eedd284f55
Mohit Ambe <mohitambe77@gmail.com>
2022-12-06 23:46:10 -0500

    Day 2

--- 02/solution.py
+++ 02/solution.py
@@ -4,13 +4,33 @@ file.close()
 
 
 def part_one():
-    print()
+    pos = [0,0]
+    for line in myin:
+        step = line[:line.index(" ")]
+        num = int(line[line.index(" ") + 1:])
+        if step == 'forward':
+            pos[0] += num
+        elif step == 'up':
+            pos[1] -= num
+        else:
+            pos[1] += num
+    print(pos[0] * pos[1])
 
 
 def part_two():
-    print()
+    pos = [0, 0, 0]
+    for line in myin:
+        step = line[:line.index(" ")]
+        num = int(line[line.index(" ") + 1:])
+        if step == 'forward':
+            pos[0] += num
+            pos[2] += num * pos[1]
+        elif step == 'up':
+            pos[1] -= num
+        else:
+            pos[1] += num
+    print(pos[0] * pos[2])
 
 
 part_one()
 part_two()
-
