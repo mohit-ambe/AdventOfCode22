01b5e1509aad0fc999606783593ef832b92e0420
Mohit Ambe <mohitambe77@gmail.com>
2022-12-09 22:54:46 -0500

    Day 6

--- 06/solution.py
+++ 06/solution.py
@@ -2,13 +2,41 @@ file = open("input.txt", "r")
 myin = [line.strip() for line in file.readlines()]
 file.close()
 
+lf = dict()
+
+
+def init_populate():
+    initial = [int(num) for num in myin[0].split(",")]
+    for i in range(0, 9):
+        lf[i] = 0
+    for num in initial:
+        lf[num] += 1
+
+
+def lanternfish():
+    repop = int(lf[0])
+
+    for i in range(0, 8):
+        lf[i] += lf[i + 1]
+        lf[i + 1] = 0
+
+    lf[0] -= repop
+    lf[6] += repop
+    lf[8] += repop
+
 
 def part_one():
-    print()
+    init_populate()
+    for i in range(80):
+        lanternfish()
+    print(sum(lf.values()))
 
 
 def part_two():
-    print()
+    init_populate()
+    for i in range(256):
+        lanternfish()
+    print(sum(lf.values()))
 
 
 part_one()
