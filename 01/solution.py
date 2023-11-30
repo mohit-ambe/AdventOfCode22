c33d37e886d61b8172688c881da0d8a3f7b84436
Mohit Ambe <mohitambe77@gmail.com>
2023-11-30 18:58:13 -0500

    .

--- 01/solution.py
+++ 01/solution.py
@@ -4,33 +4,13 @@ file.close()
 
 
 def part_one():
-    max_cals = float('-inf')
-    cals = 0
-    for num in myin:
-        if num == "":
-            if cals > max_cals:
-                max_cals = cals
-            cals = 0
-        else:
-            cals += int(num)
-
-    print(max_cals)
+    print()
 
 
 def part_two():
-    max_cals = [float('-inf'), float('-inf'), float('-inf')]
-    cals = 0
-    for num in myin:
-        if num == "":
-            if cals > max_cals[0]:
-                max_cals[0] = cals
-                max_cals.sort()
-            cals = 0
-        else:
-            cals += int(num)
-
-    print(sum(max_cals))
+    print()
 
 
 part_one()
 part_two()
+
