4c629a6b7ea1fc5fe1fd3332141d3e33d2f0c1da
Mohit Ambe <mohitambe77@gmail.com>
2023-12-01 00:56:35 -0500

    Day 1

--- 01/solution.py
+++ 01/solution.py
@@ -4,13 +4,28 @@ file.close()
 
 
 def part_one():
-    print()
+    t = 0
+    for line in myin:
+        s = ""
+        for l in line:
+            if l.isnumeric():
+                s += l
+        t += int(s[0] + s[-1])
+    print(t)
 
 
 def part_two():
-    print()
+    t = 0
+    for line in myin:
+        for i, s in enumerate(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']):
+            line = line.replace(s, s[0] + str(i + 1) + s[-1])
+        s = ""
+        for l in line:
+            if l.isnumeric():
+                s += l
+        t += int(s[0] + s[-1])
+    print(t)
 
 
 part_one()
 part_two()
-
