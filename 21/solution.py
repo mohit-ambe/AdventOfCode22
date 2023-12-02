ebe4d5536539e190079fbd7a82c5c540db073bd6
Mohit Ambe <mohitambe77@gmail.com>
2023-12-02 16:21:57 -0500

    Day 21

--- 21/solution.py
+++ 21/solution.py
@@ -1,14 +1,37 @@
+import time
+from sympy import *
+
 file = open("input.txt", "r")
 myin = [line.strip() for line in file.readlines()]
 file.close()
 
+jobs = {}
+
+for line in myin:
+    jobs[line[:line.index(":")]] = line[line.index(":") + 2:]
+
 
 def part_one():
-    print()
+    yell = "root"
+    while (any([y.isalpha() for y in yell])):
+        for j in jobs:
+            if j in yell:
+                yell = yell.replace(j, "({})".format(jobs[j]))
+    print(int(eval(yell)))
 
 
 def part_two():
-    print()
+    equation = ""
+    for term in jobs["root"].split(" "):
+        if not term.isalpha():
+            continue
+        yell = term
+        while (any([y.isalpha() for y in yell.replace("humn","")])):
+            for j in jobs:
+                if j in yell and j != "humn":
+                    yell = yell.replace(j, "({})".format(jobs[j]))
+        equation += yell + "-" #arrange the equation: a=b -> a-b=0
+    print(solve(parse_expr(equation[:-1]))[0]) #solve the express a-b=0, which is a=b
 
 
 part_one()
