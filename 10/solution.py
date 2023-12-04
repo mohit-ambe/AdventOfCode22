7c6df6ac7be8a6cf6eb9ef059b4a3bcf90eacc09
Mohit Ambe <mohitambe77@gmail.com>
2023-12-03 23:01:26 -0500

    Day 10

--- 10/solution.py
+++ 10/solution.py
@@ -2,13 +2,71 @@ file = open("input.txt", "r")
 myin = [line.strip() for line in file.readlines()]
 file.close()
 
+error = {
+    ")":3,
+    "]":57,
+    "}":1197,
+    ">":25137
+}
+
+complete = {
+    ")":1,
+    "]":2,
+    "}":3,
+    ">":4
+}
+
+incomplete = []
+
+
+def simplify(line):
+    while True:
+        old = line
+        line = line.replace("()", "")
+        line = line.replace("[]", "")
+        line = line.replace("{}", "")
+        line = line.replace("<>", "")
+        if len(line) == len(old):
+            return line
+
 
 def part_one():
-    print()
+    score = 0
+
+    for i, line in enumerate(myin):
+        line = simplify(line)
+
+        corrupt = False
+        for char in line:
+            if char not in "([{<":
+                corrupt = True
+                score += error[char]
+                break
+
+        if not corrupt:
+            incomplete.append(myin[i])
+
+    print(score)
 
 
 def part_two():
-    print()
+    scores = []
+
+    for i, line in enumerate(incomplete):
+        line = simplify(line)
+
+        line = line.replace("(", ")")
+        line = line.replace("[", "]")
+        line = line.replace("{", "}")
+        line = line.replace("<", ">")
+        score = 0
+        for char in line[::-1]:
+            score *= 5
+            score += complete[char]
+        scores.append(score)
+
+    scores.sort()
+    print(scores[len(scores) // 2])
 
 
 part_one()
