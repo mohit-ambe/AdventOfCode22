cceadc9dace56b0de7e8d490d16e97368d81a719
Mohit Ambe <mohitambe77@gmail.com>
2023-12-04 00:42:34 -0500

    Day 4

--- 04/solution.py
+++ 04/solution.py
@@ -1,14 +1,40 @@
+from functools import cache
+
 file = open("input.txt", "r")
 myin = [line.strip() for line in file.readlines()]
 file.close()
 
+cards = [line[line.index(":") + 2:] for line in myin]
+winning_numbers = []
+
 
 def part_one():
-    print()
+    points = 0
+
+    for card in cards:
+        card = card.replace("  ", " ").strip()
+        win, mine = [[int(x) for x in half.split(" ")] for half in card.split(" | ")]
+        my_winning = [num for num in win if num in mine]
+
+        winning_numbers.append(len(my_winning))
+        if my_winning:
+            points += 2 ** (len(my_winning) - 1)
+
+    print(points)
+
+
+@cache
+def play_cards(i):
+    cards_won = winning_numbers[i]
+
+    for card in range(i + 1, i + 1 + winning_numbers[i]):
+        cards_won += play_cards(card)
+
+    return cards_won
 
 
 def part_two():
-    print()
+    print(len(cards) + sum([play_cards(i) for i in range(len(cards))]))
 
 
 part_one()
