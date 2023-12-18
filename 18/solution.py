48bd2371c0e23a24512c09d648ab8382b84e68b2
Mohit Ambe <mambe@purdue.edu>
2023-12-18 14:28:36 -0500

    Day 18

--- 18/solution.py
+++ 18/solution.py
@@ -3,12 +3,41 @@ myin = [line.strip() for line in file.readlines()]
 file.close()
 
 
+def dig_trench(plan):
+    ans = 0
+    r = 0
+    c = 0
+    peri = 0
+    trench = [(0, 0)]
+    for p in plan:
+        d, m, = p
+        if d in "R0":
+            c += m
+        if d in "D1":
+            r += m
+        if d in "L2":
+            c -= m
+        if d in "U3":
+            r -= m
+        peri += m
+        trench.append((r, c))
+
+    # shoelace
+    for (x1, y1), (x2, y2) in zip(trench[:-1], trench[1:]):
+        ans += (x2 - x1) * (y1 + y2)
+    return ans // 2 + peri // 2 + 1  # pick's thm
+
+
 def part_one():
-    print()
+    plan = [(line.split(" ")[0], int(line.split(" ")[1])) for line in myin]
+    print(dig_trench(plan))
 
 
 def part_two():
-    print()
+    plan = []
+    for line in myin:
+        plan.append((line.split(" ")[-1][-2], int(line.split(" ")[-1][2:-2], base=16)))
+    print(dig_trench(plan))
 
 
 part_one()
