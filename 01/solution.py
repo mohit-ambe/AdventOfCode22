3f264a849a3624fe9712c74f31c5925808b54931
Mohit Ambe <mambe@purdue.edu>
2024-05-17 17:08:38 +0530

    Day 1

--- 01/solution.py
+++ 01/solution.py
@@ -2,34 +2,17 @@ file = open("input.txt", "r")
 myin = [line.strip() for line in file.readlines()]
 file.close()
 
+myin = [int(x) for x in myin]
+
 
 def part_one():
-    max_cals = float('-inf')
-    cals = 0
-    for num in myin:
-        if num == "":
-            if cals > max_cals:
-                max_cals = cals
-            cals = 0
-        else:
-            cals += int(num)
-
-    print(max_cals)
+    entries = list(set([x for x in myin if 2020 - x in myin]))
+    print(entries[0] * entries[1])
 
 
 def part_two():
-    max_cals = [float('-inf'), float('-inf'), float('-inf')]
-    cals = 0
-    for num in myin:
-        if num == "":
-            if cals > max_cals[0]:
-                max_cals[0] = cals
-                max_cals.sort()
-            cals = 0
-        else:
-            cals += int(num)
-
-    print(sum(max_cals))
+    entries = list(set([y for y in myin for x in myin if 2020 - x - y in myin]))
+    print(entries[0] * entries[1] * entries[2])
 
 
 part_one()
