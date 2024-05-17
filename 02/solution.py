0eb96b92ceecb207d30aeb8db9fd35814c25ef95
Mohit Ambe <mambe@purdue.edu>
2024-05-17 17:25:13 +0530

    Day 2

--- 02/solution.py
+++ 02/solution.py
@@ -2,15 +2,34 @@ file = open("input.txt", "r")
 myin = [line.strip() for line in file.readlines()]
 file.close()
 
+interval = []
+l = []
+p = []
+
+for line in myin:
+    line = line.replace("-", " ")
+    line = line.replace(":", "")
+    interval.append(tuple([int(line.split(" ")[0]), int(line.split(" ")[1])]))
+    l.append(line.split(" ")[2])
+    p.append(line.split(" ")[3])
+
 
 def part_one():
-    print()
+    valid = 0
+    for i in range(len(myin)):
+        if interval[i][0] <= p[i].count(l[i]) <= interval[i][1]:
+            valid += 1
+    print(valid)
 
 
 def part_two():
-    print()
+    valid = 0
+    for i in range(len(myin)):
+        position = p[i][interval[i][0] - 1] + p[i][interval[i][1] - 1]
+        if position.count(l[i]) == 1:
+            valid += 1
+    print(valid)
 
 
 part_one()
 part_two()
-
