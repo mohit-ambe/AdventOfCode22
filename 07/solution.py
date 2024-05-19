3146adc69468ae31a4be5215d122ee0402e1a576
Mohit Ambe <mambe@purdue.edu>
2024-05-19 07:43:22 +0530

    Day 7

--- 07/solution.py
+++ 07/solution.py
@@ -4,11 +4,37 @@ file.close()
 
 
 def part_one():
-    print()
+    lines = myin.copy()
+    Q = ['shiny gold']
+    colors = set()
+
+    while Q:
+        color = Q.pop(0)
+        colors.add(color)
+        containing = [line for line in lines if color in line and not line.startswith(color)]
+        for c in containing:
+            Q.append(c[:c.find(" bag")])
+    print(len(colors) - 1)
+
+
+def count_bag(line):
+    total = 0
+    line = line.replace(" no ", " 0 ")[:-1]
+    line = line.replace(" bags", "")
+    line = line.replace(" bag", "")
+
+    for bag in line[line.find("contain ") + 8:].split(", "):
+        num = int("".join([x for x in bag if x.isdigit()]))
+        color = "".join([x for x in bag if not x.isdigit()]).strip()
+        if color == "other":
+            continue
+        total += num + num * count_bag([line for line in myin if line.startswith(color)][0])
+
+    return total
 
 
 def part_two():
-    print()
+    print(count_bag([line for line in myin if line.startswith("shiny gold")][0]))
 
 
 part_one()
