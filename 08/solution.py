03cff5d06ca466476c81f25ddb3b85e957c3d361
Mohit Ambe <mambe@purdue.edu>
2024-05-19 07:56:32 +0530

    Day 8

--- 08/solution.py
+++ 08/solution.py
@@ -3,12 +3,41 @@ myin = [line.strip() for line in file.readlines()]
 file.close()
 
 
+def sim(lines):
+    acc = 0
+    index = 0
+    visited = []
+
+    while index not in visited:
+        if index >= len(lines):
+            return acc, True
+        visited.append(index)
+        a, b = lines[index].split(" ")
+        b = int(b)
+        if a == "jmp":
+            index += b
+            continue
+        elif a == "acc":
+            acc += b
+        index += 1
+
+    return acc, False
+
+
 def part_one():
-    print()
+    print(sim(myin)[0])
 
 
 def part_two():
-    print()
+    for i in range(len(myin)):
+        lines = myin.copy()
+        if lines[i].startswith("jmp"):
+            lines[i] = lines[i].replace("jmp", "nop")
+        elif lines[i].startswith("nop"):
+            lines[i] = lines[i].replace("nop", "jmp")
+        acc, term = sim(lines)
+        if term:
+            print(acc)
 
 
 part_one()
