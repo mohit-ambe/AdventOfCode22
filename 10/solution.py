7b3ac2a7be8b9663134489933a84bf751602f8ca
Mohit Ambe <mambe@purdue.edu>
2024-05-19 14:14:01 +0530

    Day 10

--- 10/solution.py
+++ 10/solution.py
@@ -1,14 +1,36 @@
+from functools import cache
+
 file = open("input.txt", "r")
 myin = [line.strip() for line in file.readlines()]
 file.close()
 
+myin = [int(x) for x in myin]
+myin.append(max(myin) + 3)
+myin.append(0)
+myin.sort()
+
 
 def part_one():
-    print()
+    diffs = []
+    for i in range(len(myin) - 1):
+        diffs.append(myin[i + 1] - myin[i])
+
+    print(len([x for x in diffs if x == 1]) * len([x for x in diffs if x == 3]))
+
+
+@cache
+def path(start, end):
+    ans = 0
+    if start == end:
+        return 1
+    for n in range(start + 1, start + 4):
+        if n in myin:
+            ans += path(n, end)
+    return ans
 
 
 def part_two():
-    print()
+    print(path(0, max(myin)))
 
 
 part_one()
