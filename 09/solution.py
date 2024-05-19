11cac88118c0e7c2fc9d9d203ef36bab259c6ca7
Mohit Ambe <mambe@purdue.edu>
2024-05-19 08:19:13 +0530

    Day 9

--- 09/solution.py
+++ 09/solution.py
@@ -2,13 +2,30 @@ file = open("input.txt", "r")
 myin = [line.strip() for line in file.readlines()]
 file.close()
 
+myin = [int(x) for x in myin]
+
+invalid = 0
+
 
 def part_one():
-    print()
+    global invalid
+
+    included = myin[:25]
+    for num in myin[25:]:
+        if any([x for x in included if num - x in included and x != num - x]):
+            included.append(num)
+        else:
+            invalid = num
+            print(num)
+            break
 
 
 def part_two():
-    print()
+    for size in range(2, len(myin)):
+        for i in range(len(myin) - size):
+            if sum(myin[i:i + size]) == invalid:
+                print(min(myin[i:i + size]) + max(myin[i:i + size]))
+                return
 
 
 part_one()
