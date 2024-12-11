f0067ca7c9731ec4c8d42532e9539f330ad932c6
Mohit Ambe <mohitambe77@gmail.com>
2024-12-11 01:42:51 -0500

    Day 11

--- 11/solution.py
+++ 11/solution.py
@@ -1,14 +1,48 @@
+from functools import cache
+
 file = open("input.txt", "r")
 myin = [line.strip() for line in file.readlines()]
 file.close()
 
+nums = [int(x) for x in myin[0].split()]
+
+
+@cache
+def rules(n):
+    if n == 0:
+        return (1,)
+    elif len(str(n)) % 2 == 0:
+        s = str(n)
+        return (int(s[:len(s) // 2]), int(s[len(s) // 2:]))
+    else:
+        return (2024 * n,)
+
+
+def simulate(iters, numbers):
+    stones_dict = {n:0 for n in numbers}
+    for n in numbers:
+        stones_dict[n] += 1
+
+    for _ in range(iters):
+        new_stones = {}
+        for stone, count in stones_dict.items():
+            if count:
+                for new_stone in rules(stone):
+                    if new_stone not in new_stones:
+                        new_stones[new_stone] = count
+                    else:
+                        new_stones[new_stone] += count
+        stones_dict = new_stones.copy()
+
+    return sum(stones_dict.values())
+
 
 def part_one():
-    print()
+    print(simulate(25, nums))
 
 
 def part_two():
-    print()
+    print(simulate(75, nums))
 
 
 part_one()
