7532e3d983608b811c753f3a6164e0203c4f5ddb
Mohit Ambe <mambe@purdue.edu>
2024-12-13 11:16:27 -0500

    Day 13

--- 13/solution.py
+++ 13/solution.py
@@ -1,14 +1,48 @@
+import re
+
 file = open("input.txt", "r")
 myin = [line.strip() for line in file.readlines()]
 file.close()
 
+games = []
+g = []
+for line in myin:
+    if line:
+        g.extend([int(x) for x in re.findall("\d+", line)])
+    else:
+        games.append(g)
+        g = []
+games.append(g)
+
+
+def det(m):
+    # [[m0, m1]]
+    # [[m2, m3]]
+    return m[0] * m[3] - m[1] * m[2]
+
+
+def compute(games):
+    tokens = 0
+    for game in games:
+        # Cramer's Rule
+        # 2 Var Sys Eq
+        a0 = det([game[0], game[2], game[1], game[3]])
+        a1 = det([game[4], game[2], game[5], game[3]])
+        a2 = det([game[0], game[4], game[1], game[5]])
+        if a0:
+            if a1 / a0 == a1 // a0 and a2 / a0 == a2 // a0:
+                tokens += 3 * (a1 / a0) + (a2 / a0)
+    return int(tokens)
+
 
 def part_one():
-    print()
+    print(compute(games))
 
 
 def part_two():
-    print()
+    unit = 10_000_000_000_000
+    games_converted = [[g[i] if i < 4 else g[i] + unit for i in range(6)] for g in games]
+    print(compute(games_converted))
 
 
 part_one()
