30ead3210153e7cbd04013c77b42e4c721d84127
Mohit Ambe <mambe@purdue.edu>
2024-12-19 02:04:59 -0500

    Day 19

--- 19/solution.py
+++ 19/solution.py
@@ -9,23 +9,21 @@ towels = [line for line in myin[myin.index("") + 1:]]
 
 
 @cache
-def valid(s, p1=True):
-    if not p1:
-        if len(s) == 0:
-            return 1
-    else:
+def valid(s, p1):
+    blocks = [p for p in patterns if s.startswith(p)]
+    if p1:
         if len(s) == 0:
             return True
         if len(s) == 1:
             return s in patterns
-    blocks = [p for p in patterns if s.startswith(p)]
-    if p1:
         return any(valid(s[len(b):], p1) for b in blocks)
     else:
+        if len(s) == 0:
+            return 1
         return sum([valid(s[len(b):], p1) for b in blocks])
 
 
-possible = [t for t in towels if valid(t)]
+possible = [t for t in towels if valid(t, p1=True)]
 
 
 def part_one():
